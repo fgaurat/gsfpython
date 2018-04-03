b3a24659167e39d0e05ca4d320daf2715e975413
Frédéric Gaurat <fgaurat@eolem.com>
2018-04-03 12:13:03 +0200

    hello

--- tplist.py
+++ tplist.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 
-squares = [1, 4, 9, 16, 25]
+squares = [1,4,9,   16,25]
 print(squares)
+
+print(squares[-1])
+print(squares[-1:])
+
+str="bonjour il fait beau"
+str2=['b','o','n','j']
+
+arr_str = str.split(" ")
+print (arr_str[0]+arr_str[-1])
