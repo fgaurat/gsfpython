84a6fb33b3e151f2eb2ef1156b7817369ae96ead
Frédéric Gaurat <fgaurat@eolem.com>
2018-04-04 09:59:01 +0200

    func

--- args_func.py
+++ args_func.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 
+def test_args(*arr):
+    print(arr)
+
+def test_args_dict(**arr):
+    print(arr['nom'])
+    print(arr['prenom'])
+
+
+def test_args_dict2(param1,*arr_pos,**arr_dict):
+    print(arr_pos)
+    print(arr_dict)
+
+# test_args("val_1")
+# test_args("val_1","val_2")
+
+# test_args_dict(nom="DUPONT",prenom="Robert")
+
+test_args_dict2("val_1","val_2","val_3",nom="DUPONT",prenom="Robert")
