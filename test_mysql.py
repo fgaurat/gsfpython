a07364748220a0dc118b14903d5d4577329f511d
Frédéric Gaurat <fgaurat@eolem.com>
2018-04-05 15:37:04 +0200

    func

--- test_mysql.py
+++ test_mysql.py
@@ -1,31 +1,38 @@
 # -*- coding: utf-8 -*-
 import mysql.connector
 from mysql.connector import errorcode
+import configparser
 
-config = {
-    'user': 'admin',
-    'password': 'mypass',
-    'host': 'ns349423.ip-94-23-40.eu',
-    'database': ''
-}
+if __name__ == "__main__":
 
-cnx = cur = None
-try:
-    cnx = mysql.connector.connect(**config)
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print('Something is wrong with your user name or password')
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    dict_config = {
+        'user': config['DATABASE']['user'],
+        'password': config['DATABASE']['password'],
+        'host': config['DATABASE']['host'],
+        'database': config['DATABASE']['database']
+    }
+
+
+    cnx = cur = None
+    try:
+        cnx = mysql.connector.connect(**dict_config)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Something is wrong with your user name or password')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
     else:
-        print(err)
-else:
-    cur = cnx.cursor()
-    cur.execute('show databases;')
-    for row in cur.fetchall():
-        print(row)
-finally:
-    if cur:
-        cur.close()
-    if cnx:
-        cnx.close()
+        cur = cnx.cursor()
+        cur.execute('show databases;')
+        for row in cur.fetchall():
+            print(row)
+    finally:
+        if cur:
+            cur.close()
+        if cnx:
+            cnx.close()
